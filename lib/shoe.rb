ca8dfd5e5d66998bebc244def01b53edc3550730
John Karrell Speck <speckworks@gmail.com>
2019-09-02 21:43:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,35 @@
-class Shoe # Make your shoe class here!
-  attr_accessor :brand, :color, :size, :material, :condition
+# class Shoe # Make your shoe class here!
+#   attr_accessor :brand, :color, :size, :material, :condition
   
-  def initialize(brand, color, size, material, condition)
+#   def initialize(brand, color, size, material, condition)
+#     @brand = brand
+#     @color = color
+#     @size = size
+#     @material = material
+#     @condition = condition
+#   end
+  
+#   def cobble
+#     puts "Your shoe is as good as new!"
+#     self.condition = "New"
+#   end
+# end
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
     @brand = brand
     @color = color
     @size = size
     @material = material
     @condition = condition
   end
-  
+
   def cobble
     puts "Your shoe is as good as new!"
-    self.condition = "New"
+    self.condition = "new"
   end
-end
 
+end
     
