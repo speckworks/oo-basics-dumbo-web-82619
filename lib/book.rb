0fcca3a90c885a5dc87ff2d343b435426d779e42
John Karrell Speck <speckworks@gmail.com>
2019-09-02 21:17:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,10 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title
   
-  def initialize(title)
+  def initialize(title, author, page_count)
   @title = title
+  @author = author
+  @page_count = page_count
   end
   
   def turn_page
