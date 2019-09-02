c4b83982405bf21eba0716401bd0e1c233a37b0b
John Karrell Speck <speckworks@gmail.com>
2019-09-02 21:35:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,18 @@
 class Shoe # Make your shoe class here!
-  attr_accessor 
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand, color, size, material, condiiton)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condiiton
+  end
+  
+  def cobble
+    @condition = "New"
+    puts "the shoe has been repaired"
+  end
+end
+
+    
